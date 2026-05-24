8b01175432567b897d93e5b4f344048333d389b8
kama <steamnitz@gmail.com>
2026-05-24 17:42:43 +0000

    feat(): add header

--- ocha/components/mainpage-sidebar.tsx
+++ ocha/components/mainpage-sidebar.tsx
@@ -10,24 +10,62 @@ import {
   SidebarMenuItem,
   SidebarHeader,
   useSidebar,
+  SidebarFooter,
 } from "@/components/ui/sidebar";
+import { Menu } from "@base-ui/react";
+import { GalleryVerticalEnd } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { ChevronRight, MessageCircle, Search, House } from "lucide-react";
+import {
+  ChevronRight,
+  ChevronsUp,
+  MessageCircle,
+  Search,
+  House,
+  Circle,
+} from "lucide-react";
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { BadgeCheck, Bell, CreditCard, LogOut, Sparkles } from "lucide-react";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function AppSidebar() {
   const { open } = useSidebar();
   return (
-    <Sidebar collapsible="icon">
+    <Sidebar variant="floating" collapsible="icon">
       <SidebarHeader>
-        <div className="p-2 group-data-[state=collapsed]:hidden font-bold text-xl">
-          o!cha
-        </div>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                <ChevronsUp className="size-4" />
+              </div>
+
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">o!cha</span>
+                <span className="truncate text-xs">Alpha Edition</span>
+              </div>
+
+              <Circle className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
@@ -60,52 +98,52 @@ export function AppSidebar() {
                     />
                     <CollapsibleContent className="mt-1 ml-5 style-lyra:ml-4">
                       <SidebarMenuButton>
-                        <Avatar>
-                          {" "}
-                          <AvatarImage src="https://github.com/godkama.png" />
-                        </Avatar>
-                        kama
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
                       </SidebarMenuButton>
                       <SidebarMenuButton>
-                        <Avatar>
-                          {" "}
-                          <AvatarImage src="https://github.com/virtualwinter.png" />
-                        </Avatar>
-                        vWinter
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
                       </SidebarMenuButton>
                       <SidebarMenuButton>
-                        <Avatar>
-                          {" "}
-                          <AvatarImage src="https://github.com/shadcn.png" />
-                        </Avatar>
-                        shadcn
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
                       </SidebarMenuButton>
                       <SidebarMenuButton>
-                        <Avatar>
-                          {" "}
-                          <AvatarImage src="https://github.com/shadcn.png" />
-                        </Avatar>
-                        shadcn
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
                       </SidebarMenuButton>
                       <SidebarMenuButton>
-                        <Avatar>
-                          {" "}
-                          <AvatarImage src="https://github.com/shadcn.png" />
-                        </Avatar>
-                        shadcn
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
                       </SidebarMenuButton>
                       <SidebarMenuButton>
-                        <Avatar>
-                          {" "}
-                          <AvatarImage src="https://github.com/shadcn.png" />
-                        </Avatar>
-                        shadcn
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
                       </SidebarMenuButton>
                       <SidebarMenuButton>
-                        <Avatar>
-                          <AvatarImage src="https://github.com/shadcn.png" />
-                        </Avatar>
-                        shadcn
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
+                      </SidebarMenuButton>
+                      <SidebarMenuButton>
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
+                      </SidebarMenuButton>
+                      <SidebarMenuButton>
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
+                      </SidebarMenuButton>
+                      <SidebarMenuButton>
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
+                      </SidebarMenuButton>
+                      <SidebarMenuButton>
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
+                      </SidebarMenuButton>
+                      <SidebarMenuButton>
+                        <Skeleton className="size-8 shrink-0 rounded-full" />
+                        <Skeleton className="h-4 w-37.5" />
                       </SidebarMenuButton>
                     </CollapsibleContent>
                   </Collapsible>

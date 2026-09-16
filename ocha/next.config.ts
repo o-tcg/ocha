1ecfe2ff5d04869af746e03d1606610ab359d542
kama <steamnitz@gmail.com>
2026-09-16 10:18:35 +0000

    fix(deployment): add a hotfix for github pages

--- ocha/next.config.ts
+++ ocha/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["127.0.0.1"],
+  distDir: "out", // this is the default
   reactCompiler: true,
   devIndicators: false,
   output: "export",
